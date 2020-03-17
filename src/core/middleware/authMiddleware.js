ca7fc76c7b9cbff22e264fbfe2775a787110170e
Tanya Dolgopolova <tetiana.dolgopolova@gmail.com>
2020-03-17 14:01:03 +0200

    Code cleaning

--- src/core/middleware/authMiddleware.js
+++ src/core/middleware/authMiddleware.js
@@ -4,28 +4,28 @@ import config from "config";
 let AuthMiddleware = (req, res, next) => {
     let token = req.headers["x-access-token"] || req.headers["authorization"];
 
-    if (token) {
-        if (token.startsWith("Bearer ")) {
-            token = token.slice(7, token.length);
-        }
-
-        jwt.verify(token, config.jwtSecretToken, (err, decoded) => {
-            if (err) {
-                return res.json({
-                    success: false,
-                    message: "Token is not valid"
-                });
-            } else {
-                req.user = decoded;
-                next();
-            }
-        });
-    } else {
+    if (!token) {
         return res.json({
             success: false,
             message: "Auth token is not supplied"
         });
     }
+
+    if (token.startsWith("Bearer ")) {
+        token = token.slice(7, token.length);
+    }
+
+    jwt.verify(token, config.jwtSecretToken, (err, decoded) => {
+        if (err) {
+            return res.json({
+                success: false,
+                message: "Token is not valid"
+            });
+        } else {
+            req.user = decoded;
+            next();
+        }
+    });
 };
 
 module.exports = AuthMiddleware;
